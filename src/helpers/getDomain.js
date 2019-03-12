0673417330e9341dd7caf9398b6a5df6332b5aca
Joschwa <johann.schwabe@uzh.ch>
2019-03-12 21:41:14 +0100

    fix typo in server adress

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -6,7 +6,7 @@ import { isProduction } from "./isProduction";
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = "https://sopra-fs19-schwabe-j-sercer.herokuapp.com"; // TODO: insert your groups heroku prod url for server (once deployed)
+  const prodUrl = "https://sopra-fs19-schwabe-j-server.herokuapp.com/";
   const devUrl = "http://localhost:8080";
   if (isProduction()) {
     return prodUrl;
